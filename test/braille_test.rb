9442b37b1d54a0f469e05a3a12af07b1fbc06608
Tara Craig <tara.craig7@gmail.com>
2018-07-07 09:56:28 -0600

    Added braille library, test to see if it can translate a single letter.

--- test/braille_test.rb
+++ test/braille_test.rb
@@ -9,21 +9,12 @@ class BrailleTest <  Minitest::Test
     braille = Braille.new
 
     assert_instance_of Braille, braille
-
   end
 
   def test_it_can_translate_single_characters
     braille = Braille.new
 
-    assert_equal     ,  braille.translate("h")
 
+    assert_equal ["0.", "00", ".."],  braille.translate("h")
   end
-
-
-
-
-
-
-
-
 end
