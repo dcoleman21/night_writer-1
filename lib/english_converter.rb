8e86ae405817cbf592b34f7a07189574ea20880b
Maria Torres <25314178+tmaria17@users.noreply.github.com>
2018-07-10 19:10:41 -0600

    creates tests and methods to delete and break lines and scan single characters

--- lib/english_converter.rb
+++ lib/english_converter.rb
@@ -1,7 +1,26 @@
+require_relative "./braille_converter"
+
+
 class EnglishConverter
+ attr_reader :english_library
 
+  def initialize
+    braille = BrailleConverter.new
+    @english_library = braille.braille_library.invert
 
+  end
+    def translate(braille_message)
+      deleted = delete_new_line_characters(braille_message)
+      scanned = braille_scanner(deleted)
+    end
 
+    def delete_new_line_characters(braille_message)
+        braille_message.delete("\n")
+    end
+
+    def braille_scanner(deleted)
+      deleted.scan(/.{1,2}/)
+    end
 
 
 end
--- test/english_converter_test.rb
+++ test/english_converter_test.rb
@@ -1,6 +1,7 @@
 require 'minitest/autorun'
 require 'minitest/pride'
 require './lib/english_converter'
+require 'pry'
 
 class EnglishConverterTest <  Minitest::Test
 
@@ -8,7 +9,20 @@ class EnglishConverterTest <  Minitest::Test
     english = EnglishConverter.new
 
     assert_instance_of EnglishConverter , english
+  end
 
+  def test_it_can_delete_new_line_characters
+    english = EnglishConverter.new
+
+    assert_equal "0....." , english.delete_new_line_characters("0.\n..\n..")
+  end
+
+  def test_it_can_break_up_braille_string
+
+    english = EnglishConverter.new
+    actual = english.braille_scanner("0.....")
+
+    assert_equal ["0.","..",".."], actual
   end
 
 
